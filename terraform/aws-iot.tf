d64ee0dd003d6ae2c89b54a1c7476f96b117f9c0
Christoph Birk <christoph.birk@airbus.com>
2018-05-29 08:12:16 +0200

    Add untestet elasticservice domain

--- terraform/aws-iot.tf
+++ terraform/aws-iot.tf
@@ -8,3 +8,45 @@ resource "aws_iot_thing" "example" {
     First = "temp"
   }
 }
+
+# create an elasticservice domain
+# Cognito User Pool, Cognito Identity Pool, IAM Role and users are created manually at the moment.
+resource "aws_elasticsearch_domain" "es" {
+  domain_name           = "mirj"
+  elasticsearch_version = "6.2"
+  cluster_config {
+    instance_type = "t2.small.elasticsearch"
+    instance_count = 1
+  }
+
+  advanced_options {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
+
+  ebs_options {
+    ebs_enabled = "true"
+    volume_type = "General Purpose (SSD)"
+    volume_size = "10"
+  }
+
+  access_policies = <<CONFIG
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "arn:aws:iam::671323360782:role/Cognito_elasticAuth_Role"
+          },
+          "Action": "es:*",
+          "Resource": "arn:aws:es:eu-central-1:671323360782:domain/mirj/*"
+        }
+      ]
+    }
+    CONFIG
+
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+
+ }
